3f72d4a8771cf17ff954ecb74b3b69284bddd1a4
omarabir <omarabir@gmail.com>
2025-11-12 03:17:38 +0600

    Added listings API and MongoDB connection

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to pawmart server");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
 const uri =
   "mongodb+srv://pawmart:RUyrZxRdKWmUniYR@cluster0.ojsqemw.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri, {
@@ -20,6 +28,14 @@ async function run() {
   try {
     await client.connect();
 
+    const db = client.db("pawmartDB");
+    const listingsCollection = db.collection("listings");
+
+    app.get("/listings", async (req, res) => {
+      const listings = await listingsCollection.find().toArray();
+      res.send(listings);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
@@ -28,11 +44,3 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
